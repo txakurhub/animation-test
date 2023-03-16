ae93c88075be07df25b4d230e37df6909b9775d3
Leandro Pereyra <me.leandrop@gmail.com>
2023-03-16 10:33:39 -0300

    ft audio sketch-audio

--- sketch-audio.js
+++ sketch-audio.js
@@ -1,14 +1,25 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1080, 1080],
 };
-
+let audio;
 const sketch = () => {
+  audio = document.createElement("audio");
+  audio.src = "src/assets/piruetatbnmidi.mp3";
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
   };
 };
 
+const addListeners = () => {
+  window.addEventListener("mouseup", () => {
+    if (audio.paused) audio.play();
+    else audio.pause();
+  });
+};
+
+addListeners();
 canvasSketch(sketch, settings);
