720d973d24a47c13b36d92fe81ee77a538e3891c
Leandro Pereyra <me.leandrop@gmail.com>
2023-03-16 18:51:54 -0300

    ft analyser

--- sketch-audio.js
+++ sketch-audio.js
@@ -1,25 +1,76 @@
 const canvasSketch = require("canvas-sketch");
 
+let manager;
+
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
-let audio;
+let audio, audioContext, audioData, sourceNode, analyserNode;
 const sketch = () => {
-  audio = document.createElement("audio");
-  audio.src = "src/assets/piruetatbnmidi.mp3";
-
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    if (!audioContext) return;
+    analyserNode.getFloatFrequencyData(audioData);
+
+    const avg = getAverage(audioData);
+
+    context.save();
+    context.translate(width * 0.5, height * 0.5);
+    context.lineWidth = 10;
+
+    context.beginPath();
+    context.arc(0, 0, Math.abs(avg), 0, Math.PI * 2);
+    context.stroke();
+
+    context.restore();
   };
 };
 
 const addListeners = () => {
   window.addEventListener("mouseup", () => {
-    if (audio.paused) audio.play();
-    else audio.pause();
+    if (!audioContext) createAudio();
+
+    if (audio.paused) {
+      audio.play();
+      manager.play();
+    } else {
+      audio.pause();
+      manager.pause();
+    }
   });
 };
 
-addListeners();
-canvasSketch(sketch, settings);
+const createAudio = () => {
+  audio = document.createElement("audio");
+  audio.src = "src/assets/piruetatbnmidi.mp3";
+
+  audioContext = new AudioContext();
+
+  sourceNode = audioContext.createMediaElementSource(audio);
+  sourceNode.connect(audioContext.destination);
+
+  analyserNode = audioContext.createAnalyser();
+  sourceNode.connect(analyserNode);
+
+  audioData = new Float32Array(analyserNode.frequencyBinCount);
+
+  // console.log(audioData.length);
+};
+
+const getAverage = (data) => {
+  let sum = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i];
+  }
+  return sum / data.length;
+};
+const start = async () => {
+  addListeners();
+  manager = await canvasSketch(sketch, settings);
+  manager.pause;
+};
+start();
